41ede6710ce3ff1699bb026b057fbf7b1264934d
raulv <raulv@DESKTOP-FOQPVV2>
2023-03-15 19:29:12 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_RaulGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
